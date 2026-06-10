57c723a80fc39fbfc250ba79b858700f86c8b774
takionss <realyain@gmail.com>
2026-06-11 00:43:22 +0900

    Add files via upload

---
+++ adsense-loader.js
@@ -0,0 +1,7 @@
+(function() {
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2837251751819266";
+    script.crossOrigin = "anonymous";
+    document.head.appendChild(script);
+})();
